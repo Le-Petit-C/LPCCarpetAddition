c48f6e4b683f24edc09a105e3c1479ff58db35fb
Le_Petit_C <2729362136@qq.com>
2025-05-11 23:42:58 +0800

    修改了disableAnvilPunishment的实现方式

--- src/main/java/lpcCarpetAddition/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/lpcCarpetAddition/mixin/AnvilScreenHandlerMixin.java
@@ -2,7 +2,10 @@ package lpcCarpetAddition.mixin;
 
 import lpcCarpetAddition.LPCCarpetSettings;
 import lpcCarpetAddition.mixinInterfaces.IEnchantmentDefinitionMixin;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +15,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreenHandler.class)
 public class AnvilScreenHandlerMixin {
-    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;intValue()I"))
-    int erasePunishment(Integer instance){
-        if(LPCCarpetSettings.disableAnvilPunishment) return 0;
-        else return instance;
+    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
+    Object erasePunishment(ItemStack instance, ComponentType<?> componentType, Object o){
+        if(LPCCarpetSettings.disableAnvilPunishment && componentType == DataComponentTypes.REPAIR_COST) return 0;
+        else return instance.getOrDefault(componentType, o);
     }
     @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;getMaxLevel()I"))
     int redirectGetMaxLevel(Enchantment instance){
